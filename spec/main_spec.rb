c0ba3fc82e39a3b89821d117d168e7a638787143
ouyangwenli@gmail.com <wen2007>
2021-12-28 13:29:37 +0800

    add db to disk and debug file

---
+++ spec/main_spec.rb
@@ -0,0 +1,75 @@
+describe 'database' do
+  def run_script(commands)
+    raw_output = nil
+    IO.popen("./cmake-build-debug/Sqlite_Tutorial", "r+") do |pipe|
+      commands.each do |command|
+        pipe.puts command
+      end
+
+      pipe.close_write
+
+      # Read entire output
+      raw_output = pipe.gets(nil)
+    end
+    raw_output.split("\n")
+  end
+
+  it 'inserts and retrieves a row' do
+    result = run_script([
+      "insert 1 user1 person1@example.com",
+      "select",
+      ".exit",
+    ])
+    expect(result).to match_array([
+      "db > Executed.",
+      "db > (1, user1, person1@example.com)",
+      "Executed.",
+      "db > ",
+    ])
+  end
+
+
+      it 'prints error message when table is full' do
+        script = (1..1401).map do |i|
+          "insert #{i} user#{i} person#{i}@example.com"
+        end
+        script << ".exit"
+        result = run_script(script)
+        expect(result[-2]).to eq('db > Error: Table full.')
+      end
+
+       it 'prints an error message if id is negative' do
+          script = [
+            "insert -1 cstack foo@bar.com",
+            "select",
+            ".exit",
+         ]
+          result = run_script(script)
+          expect(result).to match_array([
+            "db > ID must be positive.",
+            "db > Executed.",
+            "db > ",
+          ])
+        end
+
+        it 'keeps data after closing connection' do
+          result1 = run_script([
+            "insert 1 user1 person1@example.com",
+            ".exit",
+          ])
+          expect(result1).to match_array([
+            "db > Executed.",
+            "db > ",
+          ])
+          result2 = run_script([
+            "select",
+            ".exit",
+          ])
+          expect(result2).to match_array([
+            "db > (1, user1, person1@example.com)",
+            "Executed.",
+            "db > ",
+          ])
+        end
+
+end
